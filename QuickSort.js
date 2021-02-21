e32f78463ea1902f66385c5891b4d41b5ccca832
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-02-21 19:47:11 +0800

    quicksort in py and js

---
+++ QuickSort.js
@@ -0,0 +1,23 @@
+function quickSort(array) {
+  if (array.length < 2)
+    return array;
+  let pivot = array[0] //pivot as first element of array
+  let { bigger, smaller } = partition(array.slice(1), pivot);
+  bigger = quickSort(bigger);
+  smaller = quickSort(smaller);
+  return [...smaller, pivot, ...bigger];
+}
+
+function partition(array, pivot) {
+  let bigger = [];
+  let smaller = [];
+  for(let element of array) {
+    if (element > pivot)
+      bigger.push(element);
+    else
+      smaller.push(element);
+  }
+  return { bigger, smaller };
+}
+
+console.log(quickSort([2, 5, 3, -22, 6, -4, -5, 100, 32, 2, 34, -2323, 6, -4, 89, 69]));
--- Quicksort.py
+++ Quicksort.py
@@ -1,6 +1,21 @@
-def help():
-  print("help me")
-  
+def quickSort(array):
+  if len(array) < 2:
+    return array[:]
+  else:
+    pivot = array[0]
+    smaller, bigger = partition(array[1:], pivot)
+    smaller = quickSort(smaller)
+    bigger = quickSort(bigger)
+    return smaller + [pivot] + bigger
 
-print("hello world")
-help()
+def partition(array, pivot):
+  smaller = []
+  bigger = []
+  for item in array:
+    if item <= pivot:
+      smaller.append(item)
+    else:
+      bigger.append(item)
+  return smaller, bigger
+
+print (quickSort([3, 4, 6, 9, 100, 2, -2, -333, 34, 2, 5]))
